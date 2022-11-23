d692c76e7c2d506c3ee77bda5ae08822ff43385b
Kyle Starr <kyle.starr@icloud.com>
2022-11-23 14:19:06 -0800

    Update cropSwitchScreenshots.py

--- cropSwitchScreenshots.py
+++ cropSwitchScreenshots.py
@@ -1,6 +1,6 @@
 #! python3
-# copySwitchScreenshots.py - Crops all images in current working directory
-# to remove side bars added to 4:3 games by Switch
+# copySwitchScreenshots.py - A Python script that crops all images in  
+# current working directory to remove Nintendo Switch UI from 4:3 games
 
 import os
 from PIL import Image
